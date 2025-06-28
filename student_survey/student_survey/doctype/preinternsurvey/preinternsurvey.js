8cec93466dc2c174336cf6459a1d24f4c236fdb9
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-06-28 00:41:55 +0000

    feat: Add PreInternSurvey doctype with initial structure and tests

---
+++ student_survey/student_survey/doctype/preinternsurvey/preinternsurvey.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, shreshtadev and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("PreInternSurvey", {
+// 	refresh(frm) {
+
+// 	},
+// });
